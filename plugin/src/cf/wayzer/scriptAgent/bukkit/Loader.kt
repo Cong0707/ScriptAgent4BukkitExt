aaef28839e125003c004a871040c073b636f087b
Cong0707 <49234756+Cong0707@users.noreply.github.com>
2023-06-09 17:47:37 +0800

    check casting

--- plugin/src/cf/wayzer/scriptAgent/bukkit/Loader.kt
+++ plugin/src/cf/wayzer/scriptAgent/bukkit/Loader.kt
@@ -4,6 +4,7 @@ import cf.wayzer.libraryManager.Dependency
 import cf.wayzer.libraryManager.LibraryManager
 import cf.wayzer.scriptAgent.ScriptAgent
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.java.JavaPluginLoader
 import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -21,18 +22,23 @@ class Loader : JavaPlugin() {
             require(Dependency("org.jetbrains.kotlin:kotlin-reflect:${ScriptAgent.kotlinVersion}"))
         }.createSelfFirstClassloader(null) {
             if (it == null) return@createSelfFirstClassloader false
-            (it as java.lang.String).startsWith("cf.wayzer.scriptAgent")
-                    || (it as java.lang.String).startsWith("com.google.common")
-                    || (it as java.lang.String).startsWith("io.netty")
+            it.startsWith("cf.wayzer.scriptAgent")
+                    || it.startsWith("com.google.common")
+                    || it.startsWith("io.netty")
         }?.apply {
             loadClass("cf.wayzer.scriptAgent.ScriptAgent")
                 .getMethod("afterStdLib", Path::class.java)
                 .invoke(null, libraryPath)
         }
-        impl = classLoader?.loadClass("cf.wayzer.scriptAgent.bukkit.Main")
-            ?.getConstructor(JavaPlugin::class.java, File::class.java)
-            ?.newInstance(this, file) as? JavaPlugin
-            ?: error("Fail newInstance")
+        val loader = this.pluginLoader as? JavaPluginLoader
+        if (loader != null) {
+            impl = classLoader?.loadClass("cf.wayzer.scriptAgent.bukkit.Main")
+                    ?.getConstructor(JavaPluginLoader::class.java, JavaPlugin::class.java, File::class.java)
+                    ?.newInstance(this, file) as? JavaPlugin
+                    ?: error("Fail newInstance")
+        }else{
+            error("Fail casting")
+        }
     }
 
     override fun onLoad() {
